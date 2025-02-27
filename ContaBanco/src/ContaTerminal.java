e2d53336a1f5addcfd7663f2551827fc16aaf7eb
joelmirmanoel <joelmirsilva80@outlook.com>
2025-02-27 00:01:38 -0300

    finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,7 +5,7 @@ public class ContaTerminal {
        
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Por favor, digite o número da Agência! ");
+        System.out.println("Seja bem vindo! \nPor favor, digite o número da Agência! ");
         String agencia = scan.next();
 
         System.out.println("Por favor, digite o número da Conta. ");
@@ -17,6 +17,6 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o saldo. ");
         double saldo = scan.nextDouble();
 
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + conta + " 220-1e seu saldo " + saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
